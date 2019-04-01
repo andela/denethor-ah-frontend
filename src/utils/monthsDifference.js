6e1bf792c64d0f74dc808a8beb6ad3d30a5d9310
John Samuel <jsamchineme@gmail.com>
2019-04-01 11:28:53 +0100

    Add Article Create

--- src/utils/monthsDifference.js
+++ src/utils/monthsDifference.js
@@ -1,7 +1,7 @@
 export const monthDifference = (createdAt) => {
   let numberOfMonths;
   let date1 = new Date(createdAt.split('T')[0]);
-  let date2 = new Date()
+  let date2 = new Date();
   numberOfMonths= (date2.getFullYear() - date1.getFullYear()) * 12;
   numberOfMonths-= date1.getMonth() + 1;
   numberOfMonths+= date2.getMonth(); 
